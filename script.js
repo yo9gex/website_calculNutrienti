174fd8c1ddbebe8847feea66a36e5dd46dedb201
yo9gex <60942560+yo9gex@users.noreply.github.com>
2025-11-14 14:01:05 +0200

    Enhance navigation toggle functionality
    Added event listeners to nav links to remove active class.

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 const navToggleBtn = document.querySelector('#navToggleBtn');
 const nav = document.querySelector('nav.navigation');
-
+const navLinks = document.querySelectorAll('nav .nav-link');
 
 navToggleBtn.addEventListener('click', () =>{
     nav.classList.toggle('active');
 })
+
+navLinks.forEach(link => {
+    link.addEventListener('click' , () => {
+    nav.classList.remove('active');
+    });
+});
